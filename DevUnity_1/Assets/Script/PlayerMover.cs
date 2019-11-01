5b9f756dd889ce2433b326808d406344674db1c1
Yuuki Bamba <yuukiBamba@gmail.com>
2019-11-01 11:46:15 +0900

    update dev_1 project.

---
+++ DevUnity_1/Assets/Script/PlayerMover.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 0.1f; //  プレイヤーの移動速度
+    [SerializeField]
+    float jumpPower = 50.0f;//  プレイヤーのジャンプ力
+
+    Rigidbody rb;  //   物理オブジェクト
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  移動処理
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-moveSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(moveSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -moveSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(0, jumpPower, 0);
+        }
+    }
+}
