0af068c25bf0de0524e11de6cf98640c5244a26a
Yuuki Bamba <yuukiBamba@gmail.com>
2019-11-15 11:44:02 +0900

    第二回課題終了。

---
+++ DevUnity_1/Assets/Script/CameraControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  ターゲットを注視
+        transform.LookAt(target);
+
+        //  カメラの前後反転
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Vector3 lPos = transform.localPosition;
+            transform.localPosition = new Vector3(lPos.x, lPos.y, -lPos.z);
+        }
+    }
+}
--- DevUnity_1/Assets/Script/PlayerMover.cs
+++ DevUnity_1/Assets/Script/PlayerMover.cs
@@ -8,38 +8,87 @@ public class PlayerMover : MonoBehaviour
     float moveSpeed = 0.1f; //  プレイヤーの移動速度
     [SerializeField]
     float jumpPower = 50.0f;//  プレイヤーのジャンプ力
-
+    [SerializeField]
+    float rotspeed = 10.0f;
+    bool isGround = true;
     Rigidbody rb;  //   物理オブジェクト
-
+    Animator animator;
+    int faceIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float speed = 0, rot = 0;
+
         //  移動処理
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(-moveSpeed, 0, 0);
+            rot = -1;
+            transform.Rotate(0, -rotspeed * Time.deltaTime, 0);
+            //transform.Translate(-moveSpeed, 0, 0);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(moveSpeed, 0, 0);
+            rot = 1;
+            transform.Rotate(0, rotspeed * Time.deltaTime, 0);
+            //transform.Translate(moveSpeed, 0, 0);
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(0, 0, moveSpeed);
+            speed = Input.GetKey(KeyCode.LeftShift) ? 0.5f : 1.0f;
+            transform.Translate(0, 0, (moveSpeed * Time.deltaTime) * speed);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(0, 0, -moveSpeed);
+            speed = -0.5f;
+            transform.Translate(0, 0, (-moveSpeed * Time.deltaTime) * speed);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
         {
             rb.AddForce(0, jumpPower, 0);
+            animator.SetBool("Jump", true);
         }
+
+        //anim param
+        animator.SetFloat("speed", speed);
+        animator.SetFloat("rot", rot);
+
+        //face
+        FaceUpdate();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGround = true;
+        animator.SetBool("Jump", false);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGround = false;
+    }
+
+    void FaceUpdate()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            faceIndex = faceIndex == 1 ? 0 : 1;
+            Debug.Log("index:"+faceIndex);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            faceIndex = faceIndex == 2 ? 0 : 2;
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            faceIndex = faceIndex == 3 ? 0 : 3;
+        }
+        animator.SetInteger("FaceIndex", faceIndex);
     }
 }
